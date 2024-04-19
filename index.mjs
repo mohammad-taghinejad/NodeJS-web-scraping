f77dcdec1b027635fc259ab14ffe63f18dfafe68
Mohammad Taghinejad <smt.pv.page@gmail.com>
2024-04-19 22:31:23 +0330

    Feat: The express server was created.

--- index.mjs
+++ index.mjs
@@ -1,9 +1,29 @@
 import { load } from 'cheerio';
+import express from 'express';
 
-const response = await fetch('https://en.wikipedia.org/wiki/Node.js');
-const data = await response.text();
+const app = express();
 
-const $ = load(data);
-const summery =  $('div#mw-content-text > div.mw-parser-output > table + p:first-of-type').text();
+app.get('/topic/:topic', async (req, res) => {
+    if (!req.params.topic) {
+        res.status(404).send('Topic not found!!!');
+        return;
+    }
 
-console.log("Wikipedia summery:\n" + summery);
+    const response = await fetch(`https://en.wikipedia.org/wiki/${req.params.topic}`);
+    const data = await response.text();
+
+    const $ = load(data);
+    const summery = $('div#mw-content-text > div.mw-parser-output > table.infobox + p').text();
+
+    if (summery.length < 1) {
+        res.status(404).send(`Topic "${req.params.topic}" not found!`);
+        return;
+    }
+
+    res.send(summery);
+});
+
+
+app.listen(3000, () => {
+    console.log("The Wikipedia summery is listening on port 3000.");
+});
